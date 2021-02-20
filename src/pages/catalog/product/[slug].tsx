7f2b2c12e2f909ed89a236da65c1968ee29cef3f
Henrique Hendel <henriquehendell@gmail.com>
2021-02-19 23:57:57 -0300

    feat: hanle with routes and dynamic routing

---
+++ src/pages/catalog/product/[slug].tsx
@@ -0,0 +1,13 @@
+import {useRouter} from 'next/router'
+
+export default function Product() {
+    const router = useRouter()
+
+    const {slug} = router.query
+    
+    return (
+        <h1>
+            {slug}
+        </h1>
+    )
+}
